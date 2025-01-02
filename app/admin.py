68f06a556b1c192cf6bc4116de35c95c988bca8b
tanchicofrancis <tanchicof@gmail.com>
2025-01-02 12:35:14 +0800

    Initial commit

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Post, Product, Category, Customer, Order, OrderItem
 
-admin.site.register(Post)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Customer)
